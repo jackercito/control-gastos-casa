78bb40f0807ae52dae51df787eb059de6d946ee0
Pablo Alonso <jackercito@gmail.com>
2020-12-15 14:22:10 +0100

    commit - update package.json - actualizacion mlab a mongodb atlas

--- src/server/db/conexion.mongodb.js
+++ src/server/db/conexion.mongodb.js
@@ -5,17 +5,10 @@ const USER = process.env.MONGODB_USER;
 const PASSWORD = process.env.MONGODB_PASSWORD;
 const SERVER = process.env.MONGO_SERVER;
 
-console.log(`mongodb+srv://${USER}:${PASSWORD}@${SERVER}`);
-
 //Conexion a la base de datos
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@${SERVER}`, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000 }, async (err, db) => {
+mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@${SERVER}`, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 5000 }, (err, db) => {
   if (err) console.log("Error: " + err);
-
-  var h = require("../services/hipoteca.service")
-  var hs = await h.getHipotecas({})
-  console.log(hs);
-
 });
 
 exports.mongoose = mongoose;
