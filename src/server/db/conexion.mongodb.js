0dbdb8d4c863a763723cf7fc9139bee74f571e6b
Pablo Alonso <jackercito@gmail.com>
2020-12-15 13:51:29 +0100

    commit

--- src/server/db/conexion.mongodb.js
+++ src/server/db/conexion.mongodb.js
@@ -3,12 +3,12 @@ require('dotenv').config()
 
 const USER = process.env.MONGODB_USER;
 const PASSWORD = process.env.MONGODB_PASSWORD;
-const DB = process.env.MONGO_DB;
 const SERVER = process.env.MONGO_SERVER;
-const PORT = process.env.MONGO_PORT;
 
 //Conexion a la base de datos
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@${SERVER}`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@${SERVER}`, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+  if (err) console.log("Error: " + err);
+});
 
 exports.mongoose = mongoose;
